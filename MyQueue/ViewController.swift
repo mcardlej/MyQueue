9d7dd8a88f1474de4f86d3afce5bedaa5823b51d
ChrisDidThis <Chris.Jasson@Gmail.com>
2015-01-14 22:47:56 +1030

    Initial Commit

---
+++ MyQueue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyQueue
+//
+//  Created by Han Solo on 14/01/2015.
+//  Copyright (c) 2015 MyQueueApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
