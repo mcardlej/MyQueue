021bdf158ebe2fc77ea34c9ce60aef4a39194c56
ChrisDidThis <Chris.Jasson@Gmail.com>
2015-01-18 23:10:24 +1030

    Code commenting

--- MyQueue/QueueItem.swift
+++ MyQueue/QueueItem.swift
@@ -8,16 +8,32 @@
 
 import Foundation
 
+
+/**
+*  Data representing an Item on the Queue
+*/
 class QueueItem  {
+    /// The Persons Name
     var name        = String()
+    
+    /// Their Contact Phone Number
     var phoneNumber = String()
+    
+    /// The Time they were added to the Queue
     var timeAdded   = NSDate()
     
+    
+    /**
+    Init method for the QueueItem Class
+    
+    :param: name        zero length NSString
+    :param: phoneNumber zero length NSString
+    
+    :returns: New QueueItem
+    */
     init(name:String = "", phoneNumber:String = "") {
         self.name = name
         self.phoneNumber = phoneNumber
         self.timeAdded = NSDate()
     }
-    
-   
 }
